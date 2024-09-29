4ded9fd3fe8928b99c5d0f4503a597d06446868d
Awdhesh Kumar <awdheshkumarab98@gmail.com>
2024-09-29 18:29:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors({
-    origin: 'http://localhost:3000', // React app URL
+    origin: 'https://main--raag-dhun-music-web-app.netlify.app', // React app URL
     methods: 'GET,POST,PUT,DELETE,PATCH',
 }));
 
