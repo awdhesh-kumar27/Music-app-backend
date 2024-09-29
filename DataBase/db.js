93a46e79a698bb6f494863490e94779ccc8e4fe8
Awdhesh Kumar <awdheshkumarab98@gmail.com>
2024-09-29 17:26:59 +0530

    Update db.js
    db_url

--- DataBase/db.js
+++ DataBase/db.js
@@ -1,13 +1,14 @@
     import mongoose from 'mongoose'
+    import dotenv from 'dotenv';
+
+
+dotenv.config({ path: 'config.env' });
 mongoose.set('strictQuery', true);
-// import dotenv from 'dotenv';
-// Password :1hBAl6UQSuc2KKOE
-// mongodb+srv://awdheshkumarab98:<password>@cluster0.yxtyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
-// dotenv.config()
-
+const db_url = process.env.DB_URL;
 const connection =async()=>{
-    const URL="mongodb+srv://awdheshkumarab98:1hBAl6UQSuc2KKOE@cluster0.yxtyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    
+    const URL = db_url;
     
     try{
         await mongoose.connect(URL, {useNewUrlParser:true}) 
